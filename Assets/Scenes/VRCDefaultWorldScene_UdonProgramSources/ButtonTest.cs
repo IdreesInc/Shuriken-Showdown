6af82623c9c12f4b54697adbe455d5bf3c15fe36
Idrees Hassan <idrees@idreesinc.com>
2024-09-27 17:36:18 -0400

    Make sphere interactable

---
+++ Assets/Scenes/VRCDefaultWorldScene_UdonProgramSources/ButtonTest.cs
@@ -0,0 +1,18 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class ButtonTest : UdonSharpBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    public override void Interact()
+    {
+        Debug.Log("Object interacted!");
+    }
+}
