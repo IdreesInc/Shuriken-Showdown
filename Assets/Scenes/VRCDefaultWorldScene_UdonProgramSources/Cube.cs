41b1af7690141c125d5ee2464472d731d70230bd
Idrees Hassan <idrees@idreesinc.com>
2024-09-27 17:28:12 -0400

    Make cube interactable

---
+++ Assets/Scenes/VRCDefaultWorldScene_UdonProgramSources/Cube.cs
@@ -0,0 +1,31 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+public class Cube : UdonSharpBehaviour
+{
+    private bool isPickedUp = false;
+    private VRCPlayerApi localPlayer;
+
+    void Start()
+    {
+        localPlayer = Networking.LocalPlayer;
+    }
+
+    public override void OnPickup()
+    {
+        if (localPlayer != null && localPlayer == Networking.GetOwner(gameObject))
+        {
+            isPickedUp = true;
+        }
+    }
+
+    public override void OnDrop()
+    {
+        if (localPlayer != null && localPlayer == Networking.GetOwner(gameObject))
+        {
+            isPickedUp = false;
+        }
+    }
+}
