47866d47d87c09176816a4828d8a75406525358f
Idrees Hassan <idrees@idreesinc.com>
2024-09-27 18:25:25 -0400

    Revert "Shrink cube"
    This reverts commit 7138d3f4a9c1590d77f2fb8b17c0724ad82da0a2.

--- Assets/Scenes/VRCDefaultWorldScene_UdonProgramSources/ButtonTest.cs
+++ Assets/Scenes/VRCDefaultWorldScene_UdonProgramSources/ButtonTest.cs
@@ -6,27 +6,13 @@ using VRC.Udon;
 
 public class ButtonTest : UdonSharpBehaviour
 {
-    public GameObject objectToSpawn;  // The prefab to spawn (with VRC_Pickup and VRC_ObjectSync attached)
-    public Vector3 handOffset = new Vector3(0.1f, 0.1f, 0.1f); // Adjust to spawn near the player's hand
+    void Start()
+    {
+        
+    }
 
     public override void Interact()
     {
-        VRCPlayerApi interactingPlayer = Networking.LocalPlayer;
-
-        // Ensure the object to spawn is set
-        if (objectToSpawn != null && interactingPlayer != null)
-        {
-            // Spawn the object
-            GameObject spawnedObject = VRCInstantiate(objectToSpawn);
-            
-            // Spawn the object at an offset relative to the player's hand (you can adjust this)
-            Vector3 spawnPosition = interactingPlayer.GetPosition() + interactingPlayer.GetRotation() * handOffset;
-            spawnedObject.transform.position = spawnPosition;
-            spawnedObject.transform.rotation = interactingPlayer.GetRotation();
-        }
-        else
-        {
-            Debug.LogError("Object to spawn or interacting player is not set.");
-        }
+        Debug.Log("Object interacted!");
     }
 }
