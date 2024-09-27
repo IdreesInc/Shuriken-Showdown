b7cb92c218e4c91433d4019e956782612511a235
Idrees Hassan <idrees@idreesinc.com>
2024-09-27 19:00:58 -0400

    Working on physics

---
+++ Assets/Scenes/VRCDefaultWorldScene_UdonProgramSources/InnerCollider.cs
@@ -0,0 +1,13 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class InnerCollider : UdonSharpBehaviour
+{
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("Collided with: " + collision.gameObject.name);
+    }
+}
