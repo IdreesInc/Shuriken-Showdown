e272727dfc4070bacebc624d405cb54deadb329b
Idrees Hassan <idrees@idreesinc.com>
2024-09-28 12:41:08 -0400

    Attach script again

--- Assets/UdonSharp/Shuriken.cs
+++ Assets/UdonSharp/Shuriken.cs
@@ -4,10 +4,12 @@ using UnityEngine;
 using VRC.SDKBase;
 using VRC.Udon;
 
-public class Shuriken : UdonSharpBehaviour
-{
-    void Start()
-    {
-    
+public class Shuriken : UdonSharpBehaviour {
+    void Start() {
+        Debug.Log("Shuriken has been spawned.");
+    }
+
+    public override void OnPickup() {
+        Debug.Log("Object has been gripped.");
     }
 }
