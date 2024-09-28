9824d4c0d5db3c8f46c85daa997a1074f5bb0c4e
Idrees Hassan <idrees@idreesinc.com>
2024-09-28 11:54:52 -0400

    Get button working again

--- Assets/Scenes/VRCDefaultWorldScene_UdonProgramSources/ButtonTest.cs
+++ Assets/Scenes/VRCDefaultWorldScene_UdonProgramSources/ButtonTest.cs
@@ -6,13 +6,27 @@ using VRC.Udon;
 
 public class ButtonTest : UdonSharpBehaviour
 {
-    void Start()
-    {
-        
-    }
+    public GameObject objectToSpawn;  // The prefab to spawn (with VRC_Pickup and VRC_ObjectSync attached)
+    public Vector3 handOffset = new Vector3(0.1f, 0.1f, 0.1f); // Adjust to spawn near the player's hand
 
     public override void Interact()
     {
-        Debug.Log("Object interacted!");
+        VRCPlayerApi interactingPlayer = Networking.LocalPlayer;
+
+        // Ensure the object to spawn is set
+        if (objectToSpawn != null && interactingPlayer != null)
+        {
+            // Spawn the object
+            GameObject spawnedObject = VRCInstantiate(objectToSpawn);
+            
+            // Spawn the object at an offset relative to the player's hand (you can adjust this)
+            Vector3 spawnPosition = interactingPlayer.GetPosition() + interactingPlayer.GetRotation() * handOffset;
+            spawnedObject.transform.position = spawnPosition;
+            spawnedObject.transform.rotation = interactingPlayer.GetRotation();
+        }
+        else
+        {
+            Debug.LogError("Object to spawn or interacting player is not set.");
+        }
     }
 }
