211bd95988a924c176e6c10eb308046c7fb34a99
Idrees Hassan <idrees@idreesinc.com>
2025-09-27 11:35:44 -0400

    Create test station

---
+++ Assets/UdonSharp/StationTest.cs
@@ -0,0 +1,21 @@
+﻿using UdonSharp;  
+using UnityEngine;  
+using VRC.SDKBase;  
+  
+public class StationTest : UdonSharpBehaviour  
+{  
+    public override void Interact()  
+    {  
+        Networking.LocalPlayer.UseAttachedStation();  
+    }  
+  
+    public override void OnStationEntered(VRCPlayerApi player)  
+    {  
+        Debug.Log($"{player.displayName} Entered");  
+    }  
+  
+    public override void OnStationExited(VRCPlayerApi player)  
+    {  
+        Debug.Log($"{player.displayName} Exited");  
+    }  
+}
