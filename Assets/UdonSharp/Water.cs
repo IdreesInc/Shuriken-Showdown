e10ae32b3956c0cb9e05f86b843bc56ef99f01ef
Idrees Hassan <idrees@idreesinc.com>
2025-10-01 18:32:02 -0400

    Add bob effect to water

---
+++ Assets/UdonSharp/Water.cs
@@ -0,0 +1,22 @@
+﻿using UdonSharp;
+using UnityEngine;
+public class Water : UdonSharpBehaviour
+{
+    private readonly float BOB_HEIGHT = 0.25f;
+    private readonly float BOB_SPEED = 1.0f;
+
+    private Vector3 startPosition;
+    private float timeOffset;
+    
+    void Start()
+    {
+        startPosition = transform.position;
+        timeOffset = Random.Range(0f, 2f * Mathf.PI);
+    }
+    
+    void Update()
+    {
+        float newY = startPosition.y + Mathf.Sin((Time.time * BOB_SPEED) + timeOffset) * BOB_HEIGHT;
+        transform.position = new Vector3(startPosition.x, newY, startPosition.z);
+    }
+}
